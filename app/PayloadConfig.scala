2cf592daa556fe91fdf54bff43ba14489b9ec158
willh <william.hamill@gmail.com>
2012-10-26 23:43:10 +0200

    restructured config env getter

--- app/PayloadConfig.scala
+++ app/PayloadConfig.scala
@@ -5,6 +5,11 @@ object PayloadConfig {
   lazy val defaultUrl = "mongodb://localhost:27017"
 
   def getMongoURL = {
-    Option(System.getenv("MONGOHQ_URL")) getOrElse defaultUrl
+    val mongoUrl = System.getenv("MONGOHQ_URL")
+
+    if (mongoUrl.isEmpty)
+      defaultUrl
+    else
+      mongoUrl
   }
 }
