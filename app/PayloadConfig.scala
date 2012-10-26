bb0d18dc54553e72d9978700a8b52c3773798c75
willh <william.hamill@gmail.com>
2012-10-26 23:32:33 +0200

    replaced config var ref with env string

--- app/PayloadConfig.scala
+++ app/PayloadConfig.scala
@@ -2,10 +2,9 @@ package AppConfig
 
 object PayloadConfig {
 
-  lazy val mongoEnvProp = "MONGOHQ_URL"
   lazy val defaultUrl = "mongodb://localhost:27017"
 
   def getMongoURL = {
-    Option(System.getenv(mongoEnvProp)) getOrElse defaultUrl
+    Option(System.getenv("MONGOHQ_URL")) getOrElse defaultUrl
   }
 }
